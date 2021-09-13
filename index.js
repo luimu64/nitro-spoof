20238056eab14d459b47daae81f67e86fc9de553
luimu64 <leimuraittinen@gmail.com>
2021-09-13 22:37:29 +0300

    added server and animated check so emojis only get replaced when needed

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const { inject, uninject } = require('powercord/injector');
 
 module.exports = class EmojiSpoof extends Plugin {
     async startPlugin() {
-        const emojiStore = await getModule(['getCustomEmojiById'])
+        const getCustom = await getModule(['getCustomEmojiById']);
+        const lastGuild = await getModule(['getLastSelectedGuildId']);
 
-        //override permissions to make discord show the unavailable 
+        //override functions to make discord show the unavailable 
         //emojis as available in autocomplete and in emoji picker
-        const cCheck = await getModule(['canUseEmojisEverywhere']);
+        const cCheck = await getModule(['canUseEmojisEverywhere'])
         const aCheck = await getModule(['canUseAnimatedEmojis']);
 
         cCheck.canUseEmojisEverywhere = () => {
@@ -25,23 +26,34 @@ module.exports = class EmojiSpoof extends Plugin {
             let size = 64;
             let emotelinks = [];
 
+            //find all emojis from the captured message string and return iterable containing them
             let emojiStrings = args[1].content.matchAll(/<a?:([A-Z]+):([0-9]+)>/ig);
 
             for (const emoji of emojiStrings) {
-                args[1].content = args[1].content.replace(emoji[0], "");
-                emote = emojiStore.getCustomEmojiById(emoji[2]);
-                emotelinks.push(emote["url"] + `&size=${size}`);
+                //fetch required info about the emoji
+                emote = getCustom.getCustomEmojiById(emoji[2]);
+
+                //check if emoji is normally usable or animated
+                if (emote["guildId"] != lastGuild.getLastSelectedGuildId() || emote["animated"] == true) {
+                    //remove emoji text from the captured message string
+                    args[1].content = args[1].content.replace(emoji[0], "");
+                    //push link to array
+                    emotelinks.push(emote["url"] + `&size=${size}`);
+                }
             }
 
-            console.log(args[1].content);
+            //add links to the end of the original message
             args[1].content = args[1].content + emotelinks.join("\n");
+            //set invalidEmojis to empty to prevent discord yelling to you about you not having nitro
             args[1].invalidEmojis = [];
 
+            //send modified message
             return args;
         }
 
         const messageEvents = await getModule(["sendMessage"]);
         inject("spoofSend", messageEvents, "sendMessage", (args) => {
+            //only run if message contains emojis
             if (args[1].content.match(/<a?:([A-Z]+):([0-9]+)>/i) != null) {
                 getEmojiLinks(args);
             }
