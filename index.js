bea233c801538499d430a80c284fb5dc39503bc0
luimu64 <leimuraittinen@gmail.com>
2021-09-17 23:22:05 +0300

    code refactoring

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports = class EmojiSpoof extends Plugin {
             render: settings,
         });
 
-        const getCustom = await getModule(['getCustomEmojiById']);
+        const { getCustomEmojiById } = await getModule(['getCustomEmojiById']);
         const lastGuild = await getModule(['getLastSelectedGuildId']);
 
         //override functions to make discord show the unavailable
@@ -28,29 +28,31 @@ module.exports = class EmojiSpoof extends Plugin {
             return true;
         }
 
-        function getEmojiLinks(size, args) {
-
-            let emote;
-            let emotelinks = [];
-
-            //find all emojis from the captured message string and return iterable containing them
-            let emojiStrings = args[1].content.matchAll(/<a?:(.+):([0-9]+)>/ig);
-
-            for (const emoji of emojiStrings) {
-                //fetch required info about the emoji
-                emote = getCustom.getCustomEmojiById(emoji[2]);
-
-                //check if emoji is normally usable or animated
-                if (emote["guildId"] != lastGuild.getLastSelectedGuildId() || emote["animated"] == true) {
-                    //remove emoji text from the captured message string
-                    args[1].content = args[1].content.replace(emoji[0], "");
-                    //push link to array
-                    emotelinks.push(emote["url"] + `&size=${size}`);
-                }
+        function extractEmojis(messageString) {
+            let emojiStrings = messageString.matchAll(/<a?:(\w+):(\d+)>/ig);
+            let emojiIds = [];
+            for (let emoji of emojiStrings) {
+                messageString = messageString.replace(emoji[0], '');
+                emojiIds.push(emoji[2]);
             }
+            return { content: messageString.trim(), emojis: emojiIds };
+        }
+
+        function getEmojiLinks(size, args) {
+            //find all emojis from the captured message string and return iterable containing them
+            let message = extractEmojis(args[1].content);
+            message.emojis.forEach((emojiId, i) => {
+                //fetch required info about the emoji
+                let emoji = getCustomEmojiById(emojiId);
+                //check if emoji is normally usable or animated
+                if (emoji["guildId"] != lastGuild.getLastSelectedGuildId() || emoji["animated"] == true) {
+                    //push link to array
+                    message.emojis[i] = emoji["url"] + `&size=${size}`;
+                }
+            });
 
             //add links to the end of the original message
-            args[1].content = args[1].content + emotelinks.join("\n");
+            args[1].content = message.content + message.emojis.join("\n");
             //set invalidEmojis to empty to prevent discord yelling to you about you not having nitro
             args[1].invalidEmojis = [];
 
